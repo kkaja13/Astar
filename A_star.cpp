e9a6dc7facb7800ade682da9c517abb1770b7849
kkaja13 <136073478+kkaja13@users.noreply.github.com>
2023-06-26 14:42:29 +0200

    Add files via upload

--- A_star.cpp
+++ A_star.cpp
@@ -1,239 +1,233 @@
 #include "A_star.h"
 #include <iostream>
 #include <fstream>
-#include <queue>
-using namespace std;
 
 graph::graph(int rows, int cols)
 {
-	this->rows = rows;
-	this->cols = cols;
-	has_path = false;
+    this->rows = rows;
+    this->cols = cols;
+    has_path = false;
 
-	matrix = vector<vector<int>>(rows, vector<int>(cols, 0));
-	cells = vector<vector<Cell>>(rows, vector<Cell>(cols));
-	path = vector<Cell>();
+    matrix = vector<vector<int>>(rows, vector<int>(cols, 0));
+    cells = vector<vector<Cell>>(rows, vector<Cell>(cols));
+    path = vector<Cell>();
 }
 
 graph::graph(const graph& src)
 {
-	this->rows = src.rows;
-	this->cols = src.cols;
-	this->has_path = src.has_path;
+    this->rows = src.rows;
+    this->cols = src.cols;
+    this->has_path = src.has_path;
 }
 
 bool graph::is_valid(Cell a) const
 {
-	return ((a.x >= 0) && (a.x < rows) && (a.y >= 0) && (a.y < cols)); //pokud jsou souradnice v poradku, vraci true
+    return ((a.x >= 0) && (a.x < rows) && (a.y >= 0) && (a.y < cols)); //pokud jsou souradnice v poradku, vraci true
 }
 
-int graph::heuristic(const Cell& a, const Cell& end) 
+int graph::heuristic(const Cell& a, const Cell& end)
 {
-	//Diagonal distance
-	int dx = abs(a.x - end.x);
-	int dy = abs(a.y - end.y);
+    int dx = abs(a.x - end.x);
+    int dy = abs(a.y - end.y);
 
-	int h_straight = 10 * max(dx, dy);			//pohyb horizontalni nebo vertikalni
-	int h_diag = (14 - 2 * 10) * min(dx, dy);	//diagonalni pohyb, zaporna hodnota -> je levnejsi, ma prednost
+    int h_straight = 10 * (dx + dy);			//pohyb horizontalni nebo vertikalni
+    int h_diag = (14 - 2 * 10) * min(dx, dy);	//diagonalni pohyb, zaporna hodnota -> je levnejsi, ma prednost
 
-	return h_straight + h_diag;
+    return h_straight + h_diag;
 }
 
 void graph::updateBorderCell(int x, int y, int g_new, int h_new, int parentX, int parentY)
 {
-	cells[x][y].parentX = parentX;
-	cells[x][y].parentY = parentY;
-	cells[x][y].g = g_new;
-	cells[x][y].h = h_new;
-	cells[x][y].f = g_new + h_new;
+    cells[x][y].parentX = parentX;
+    cells[x][y].parentY = parentY;
+    cells[x][y].g = g_new;
+    cells[x][y].h = h_new;
+    cells[x][y].f = g_new + h_new;
+}
+
+void graph::update(vector<vector<bool>> &on_border, priority_queue<Cell, vector<Cell>, greater<>> & frontier,
+                          vector<vector<bool>> & visited, Cell curr, Cell end)
+{
+    //projdeme všechny možné sousedy, tj. 8 směrů
+    for (int i = -1; i <= 1; i++)
+    {
+        for (int j = -1; j <= 1; j++)
+        {
+            int next_x = curr.x + i;
+            int next_y = curr.y + j;
+
+            //nejprve zjistim, jestli ma smysl zkoumat souseda, je-li to prekazka, visited, mimo bludiste nebo curr
+
+            if (next_x < 0 || next_x >= rows || next_y < 0 || next_y >= cols) // kontroluju souradnice souseda
+            {
+                continue;
+            }
+            if ((i == 0 && j == 0) || (matrix[next_x][next_y] == 1))   //pokud jsem na pozici curr nebo je tam prekazka
+            {
+                continue;
+            }
+            if (visited[next_x][next_y])    //pokud je uz prozkoumany
+            {
+                continue;
+            }
+
+            //spočítám g a h pro buňku -> dostanu f
+            int g_new = 0; //g souseda, tj. dosavadni g od startu ke curr + vzdalenost od curr do next
+
+            if (i == 0 || j == 0)    //sousední prvek je kolmo od buňky curr
+            {
+                g_new = cells[curr.x][curr.y].g + 10;
+            }
+            else        //sousední prvek je na diagonále
+            {
+                g_new = cells[curr.x][curr.y].g + 14;
+            }
+
+            int h_new = heuristic(cells[next_x][next_y], end);
+
+            //jestli neni prvek uz na hranici nebo je-li nové g lepsi nez prvku na hranici, pridej potomka next jako vyse
+            //do fronty border pridam next, jeho rodice budou souradnice curr, spoctu f = g + h, bude to prvek na hranici
+            if (!on_border[next_x][next_y] || g_new < cells[next_x][next_y].g)
+            {
+                updateBorderCell(next_x, next_y, g_new, h_new, curr.x, curr.y);
+                frontier.push(cells[next_x][next_y]);
+                on_border[next_x][next_y] = true;
+            }
+        }
+    }
 }
 
 void graph::search(Cell start, Cell end)
 {
-	vector<vector<bool>> on_border(rows, vector<bool>(cols, false)); //matice bunek, ty s hodnotou true jsme prosli
-	vector<vector<bool>> visited(rows, vector<bool>(cols, false));  //matice bunek, ty s hodnotou true jsou na hranici
+    vector<vector<bool>> on_border(rows, vector<bool>(cols, false)); //matice bunek, ty s hodnotou true jsme prosli
+    vector<vector<bool>> visited(rows, vector<bool>(cols, false));   //matice bunek, ty s hodnotou true jsou na hranici
 
-	has_path = false;
+    has_path = false;
 
-	if (!is_valid(start) || !is_valid(end)) //kdyby nebyly souradnice startu a cile platne, nema smysl hledat cestu
-	{
-		return;
-	}
+    if (!is_valid(start) || !is_valid(end)) //kdyby nebyly souradnice startu a cile platne, nema smysl hledat cestu
+    {
+        return;
+    }
 
-	//pocatecni bunka bude mít parametry na nule
-	cells[start.x][start.y].f = 0;
-	cells[start.x][start.y].h = 0;
-	cells[start.x][start.y].g = 0;
-	cells[start.x][start.y].parentX = start.x;
-	cells[start.x][start.y].parentY = start.y;
+    //pocatecni bunka bude mít parametry na nule
+    updateBorderCell(start.x, start.y, 0, 0, start.x, start.y);
 
-	priority_queue<Cell> frontier;  //fronta bunek na hranici, na vrchu je ta s nejmensi f hodnotou, tzv. "open list"
-	frontier.push(cells[start.x][start.y]);
-	on_border[start.x][start.y] = true;
+    priority_queue<Cell, vector<Cell>, greater<>> frontier;  //fronta bunek na hranici, na vrchu je ta s nejmensi f hodnotou, tzv. "open list"
+    frontier.push(cells[start.x][start.y]);
+    on_border[start.x][start.y] = true;
 
-	while (!frontier.empty())
-	{
-		Cell curr = frontier.top(); //bunka s nejmensi f hodnotou, soucasti cesty
-		while (!frontier.empty()) //pokud je curr navstivene, priradi curr dalsi bunku ze fronty
-		{
-			if (visited[curr.x][curr.y] == true) //již navstiveny prvek vyjmu z fronty
-			{
-				frontier.pop();
-				curr = frontier.top();
-			}
-			else
-			{
-				break;
-			}
-		}
-		frontier.pop();
-		//z prvku na hranici se stane navstiveny prvek
-		on_border[curr.x][curr.y] = false;
-		visited[curr.x][curr.y] = true;
+    while (!frontier.empty())
+    {
+        Cell curr = frontier.top(); //bunka s nejmensi f hodnotou, soucasti cesty
+        frontier.pop();
+        if (visited[curr.x][curr.y])
+        {
+            continue;
+        }
 
-		if (curr.x == end.x && curr.y == end.y) //pokud je curr cíl, existuje řešení
-		{
-			has_path = true;
-			return;
-		}
+        //z prvku na hranici se stane navstiveny prvek
+        on_border[curr.x][curr.y] = false;
+        visited[curr.x][curr.y] = true;
 
-		//projdeme všechny možné sousedy, tj. 8 směrů
-		for (int i = -1; i <= 1; i++)
-		{
-			for (int j = -1; j <= 1; j++)
-			{
-				int next_x = curr.x + i;
-				int next_y = curr.y + j;
+        if (curr.x == end.x && curr.y == end.y) //pokud je curr cíl, existuje řešení
+        {
+            has_path = true;
+            return;
+        }
 
-				//nejprve zjistim, jestli ma smysl zkoumat souseda, je-li to prekazka, visited, mimo bludiste nebo curr
-
-				if (next_x < 0 || next_x >= rows || next_y < 0 || next_y >= cols) // kontroluju souradnice souseda
-				{
-					continue;
-				}
-				if ((i == 0 && j == 0) || (matrix[next_x][next_y] == 1))   //pokud jsem na pozici curr nebo je tam prekazka
-				{
-					continue;
-				}
-				if (visited[next_x][next_y] == true)    //pokud je uz prozkoumany
-				{
-					continue;
-				}
-
-				//spočítám g a h pro buňku -> dostanu f
-				int g_new = 0; //g souseda, tj. dosavadni g od startu ke curr + vzdalenost od curr do next
-
-				if (i == 0 || j == 0)    //sousední prvek je kolmo od buňky curr
-				{
-					g_new = cells[curr.x][curr.y].g + 10;
-				}
-				else        //sousední prvek je na diagonále
-				{
-					g_new = cells[curr.x][curr.y].g + 14;
-				}
-
-				int h_new = heuristic(cells[next_x][next_y], end);
-
-				//jestli neni prvek uz na hranici nebo je-li nové g lepsi nez prvku na hranici, pridej potomka next jako vyse
-				//do fronty border pridam next, jeho rodice budou souradnice curr, spoctu f = g + h, bude to prvek na hranici
-				if (!on_border[next_x][next_y] || g_new < cells[next_x][next_y].g)
-				{
-					updateBorderCell(next_x, next_y, g_new, h_new, curr.x, curr.y);
-					frontier.push(cells[next_x][next_y]);
-					on_border[next_x][next_y] = true;
-				}
-			}
-		}
-	}
+        update(on_border, frontier, visited, curr, end);
+    }
 }
 
 void graph::print()
 {
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++)
-		{
-			cout << matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
 
 void graph::write_path()
 {
-	for (size_t i = path.size(); i > 0; i--) //zpetne iteruje proslou cestu
-	{
-		cout << "(" << path[i - 1].x << "," << path[i - 1].y << ")" << " ";
-	}
-	cout << endl;
+    for (size_t i = path.size(); i > 0; i--) //zpetne iteruje proslou cestu
+    {
+        cout << "(" << path[i - 1].x << "," << path[i - 1].y << ")" << " ";
+    }
+    cout << endl;
 }
 
 void graph::make_path(Cell start, Cell end)
 {
-	int x = end.x;
-	int y = end.y;
+    int x = end.x;
+    int y = end.y;
 
-	while (x != start.x || y != start.y) //zacina v cili a postupuje smerem ke startu pomoci odkazu na rodice
-	{
-		path.push_back(cells[x][y]);	//prida aktualni bunku do vektoru path
-		int parentX = cells[x][y].parentX;
-		int parentY = cells[x][y].parentY;
-		x = parentX;
-		y = parentY;
-	}
-	path.push_back(cells[start.x][start.y]); //prida startovni bunku nakonec
+    while (x != start.x || y != start.y) //zacina v cili a postupuje smerem ke startu pomoci odkazu na rodice
+    {
+        path.push_back(cells[x][y]);	//prida aktualni bunku do vektoru path
+        int parentX = cells[x][y].parentX;
+        int parentY = cells[x][y].parentY;
+        x = parentX;
+        y = parentY;
+    }
+    path.push_back(cells[start.x][start.y]); //prida startovni bunku nakonec
 }
 
 void graph::read_graph(const string& name)
 {
-	ifstream file;
-	file.open(name);
+    ifstream file;
+    file.open(name);
 
-	if (file.is_open())
-	{
-		file >> rows >> cols;
+    if (file.is_open())
+    {
+        file >> rows >> cols;
 
-		if (rows <= 0 || cols <= 0)
-		{
-			has_path = false;
-			return;
-		}
-		matrix = vector<vector<int>>(rows, vector<int>(cols, 0)); //matice k uchování hodnot buněk
-		cells = vector<vector<Cell>>(rows, vector<Cell>(cols)); //matice k uchování parametrů buněk
+        if (rows <= 0 || cols <= 0)
+        {
+            has_path = false;
+            return;
+        }
+        matrix = vector<vector<int>>(rows, vector<int>(cols, 0)); //matice k uchování hodnot buněk
+        cells = vector<vector<Cell>>(rows, vector<Cell>(cols)); //matice k uchování parametrů buněk
 
-		int value;
-		for (int i = 0; i < rows; i++)
-		{
-			for (int j = 0; j < cols; j++)
-			{
-				file >> value;
+        int value;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                file >> value;
 
-				if (value != 0 && value != 1) //v matici 0 predstavuje volnou cestu, 1 prekazku
-				{
-					has_path = false;
-					file.close();
-					return;
-				}
-				matrix[i][j] = value;
+                if (value != 0 && value != 1) //v matici 0 predstavuje volnou cestu, 1 prekazku
+                {
+                    has_path = false;
+                    file.close();
+                    return;
+                }
+                matrix[i][j] = value;
 
-				Cell c{};     //nova bunka pro grid_cells, rodice prozatím mimo, f,g,h na nule
-				c.x = i;
-				c.y = j;
-				c.parentX = -1;
-				c.parentY = -1;
-				c.f = 0;
-				c.g = 0;
-				c.h = 0;
+                Cell c{};     //nova bunka pro grid_cells, rodice prozatím mimo, f,g,h na nule
+                c.x = i;
+                c.y = j;
+                c.parentX = -1;
+                c.parentY = -1;
+                c.f = 0;
+                c.g = 0;
+                c.h = 0;
 
-				cells[i][j] = c;
-			}
-		}
-		file.close();
-	}
-	else
-	{
-		has_path = false;
-		return;
-	}
+                cells[i][j] = c;
+            }
+        }
+        file.close();
+    }
+    else
+    {
+        has_path = false;
+        return;
+    }
 }
+
+
--- A_star.h
+++ A_star.h
@@ -2,49 +2,52 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <queue>
 using namespace std;
 
 struct Cell
 {
-	int x, y, parentX, parentY;		//souradnice bunky a rodice
-	int f, g, h; //f = g + h, g je vzdalenost od startu k dane bunce, h je heuristicka vzdalenost od dane bunky k cili
+    int x, y, parentX, parentY;		//souradnice bunky a rodice
+    int f, g, h; //f = g + h, g je vzdalenost od startu k dane bunce, h je heuristicka vzdalenost od dane bunky k cili
 
-	Cell(int x, int y, int parentX = 0, int parentY = 0, int f = 0, int g = 0, int h = 0)
-		: x(x), y(y), parentX(parentX), parentY(parentY), f(f), g(g), h(h) {}
+    Cell() = default;
 
-	bool operator<(const Cell& other) const //porovnani f hodnot, na vrchu prioritni fronty bude bunka s nejmensim f
-	{
-		return f > other.f;
-	}
-	Cell() = default;
+    Cell(int x, int y, int parentX = 0, int parentY = 0, int f = 0, int g = 0, int h = 0)
+            : x(x), y(y), parentX(parentX), parentY(parentY), f(f), g(g), h(h) {}
+
+    bool operator>(const Cell& other) const //porovnani f hodnot, na vrchu prioritni fronty bude bunka s nejmensim f
+    {
+        return f > other.f;
+    }
 };
 
 class graph
 {
 private:
-	vector<vector<int>> matrix;         //dvourozmerne pole, bludiste
-	vector<vector<Cell>> cells;  //matice jednotlivych bunek v bludisti
-	vector<Cell> path;		//vektor obsahujici bunky cesty od startu do cile
-	int rows, cols;
+    vector<vector<int>> matrix;         //dvourozmerne pole, bludiste
+    vector<vector<Cell>> cells;  //matice jednotlivych bunek v bludisti
+    vector<Cell> path;		//vektor obsahujici bunky cesty od startu do cile
+    int rows, cols;
 
 public:
-	graph(int rows, int cols);
-	graph(int rows, int cols, bool has_path, const vector<vector<int>>& grid, const vector<vector<Cell>>& grid_cells)
-		: rows(rows), cols(cols), has_path(has_path), matrix(grid), cells(grid_cells) {}
-	graph(const graph& src);
-	~graph() = default;
+    graph(int rows, int cols);
+    graph(int rows, int cols, bool has_path, const vector<vector<int>>& grid, const vector<vector<Cell>>& grid_cells)
+            : rows(rows), cols(cols), has_path(has_path), matrix(grid), cells(grid_cells) {}
+    graph(const graph& src);
+    ~graph() = default;
 
-	bool has_path;
+    bool has_path;
 
-	bool is_valid(Cell a) const;	//jsou-li souradnice bunky v rozsahu bludište
-	int heuristic(const Cell& a, const Cell& end);	//vypocet heurestiky, hodnoty h
-	void updateBorderCell(int x, int y, int g_new, int h_new, int parentX, int parentY); //pøiøadí rodièe k buòce, stejnì jako g,h,f parametry
-	void search(Cell start, Cell end);	//nalezne nejkratsi cestu od startu do cíle
+    bool is_valid(Cell a) const;	//jsou-li souradnice bunky v rozsahu bludište
+    int heuristic(const Cell& a, const Cell& end);	//vypocet heurestiky, hodnoty h
+    void updateBorderCell(int x, int y, int g_new, int h_new, int parentX, int parentY); //pøiøadí rodièe k buòce, stejnì jako g,h,f parametry
+    void search(Cell start, Cell end);	//nalezne nejkratsi cestu od startu do cíle
+    void update(vector<vector<bool>> & on_border, priority_queue<Cell, vector<Cell>, greater<>> & frontier,
+                       vector<vector<bool>> & visited, Cell curr, Cell end);
 
-	void print();							//vypise graf (matici)
-	void make_path(Cell start, Cell end);	//zpetne zkonstruuje cestu vedouci od cile ke startu
-	void write_path();						//vypise souradnice proslych bunek
+    void print();							//vypise graf (matici)
+    void make_path(Cell start, Cell end);	//zpetne zkonstruuje cestu vedouci od cile ke startu
+    void write_path();						//vypise souradnice proslych bunek
 
-	void read_graph(const string& name); //nacte graf ze souboru txt
+    void read_graph(const string& name); //nacte graf ze souboru txt
 };
-
